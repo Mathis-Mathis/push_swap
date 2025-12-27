a2fd80f368b031cd2d6930761b71df88017b73bf
Mathis-Mathis <mathisu.mousli@gmail.com>
2025-12-27 16:54:26 +0100

    get argv

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: mmousli <mmousli@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/20 15:11:56 by mmousli           #+#    #+#             */
-/*   Updated: 2025/12/27 16:27:25 by mmousli          ###   ########.fr       */
+/*   Updated: 2025/12/27 16:54:06 by mmousli          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,14 +20,37 @@ int main(int argc, char **argv)
 	t_stack *stack_a = NULL;
 	t_stack *stack_b = NULL;
 	t_stack *temp;
+	int size;
+	int i;
+	int value;
+
+	i = 0;
+	size = argc - 1;
+
+	while (i < size)
+	{
+		value = atoi(argv[i + 1]);
+		t_stack *new_node = stack_new(value);
+		if (!stack_a)
+		{
+			stack_a = new_node;
+		}
+		else
+		{
+			t_stack *last_node = stack_last(stack_a);
+			last_node->next = new_node;
+		}
+		i++;
+	}
+
 
 	// stack a
-	stack_a = stack_new(1);
-	stack_last(stack_a)->next = stack_new(2);
-	stack_last(stack_a)->next = stack_new(3);
-	stack_last(stack_a)->next = stack_new(4);
-	stack_last(stack_a)->next = stack_new(5);
-	stack_last(stack_a)->next = stack_new(6);
+	// stack_a = stack_new(1);
+	// stack_last(stack_a)->next = stack_new(2);
+	// stack_last(stack_a)->next = stack_new(3);
+	// stack_last(stack_a)->next = stack_new(4);
+	// stack_last(stack_a)->next = stack_new(5);
+	// stack_last(stack_a)->next = stack_new(6);
 
 
 	// stack b
@@ -51,7 +74,7 @@ int main(int argc, char **argv)
 	//rotate_both(&stack_a, &stack_b);
 
 	// reverse rotate
-	reverse_rotate_a(&stack_a);
+	// reverse_rotate_a(&stack_a);
 	// reverse_rotate_b(&stack_b);
 	// reverse_rotate_both(&stack_a, &stack_b);
 
